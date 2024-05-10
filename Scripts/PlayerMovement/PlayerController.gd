c0686bc3d1f324eb0859d726a08fc27320ecd9ad
fozzedout <fozzedout@gmail.com>
2024-05-10 23:47:13 +0100

    changes to the protoship so the animation is applied correctly

--- Scripts/PlayerMovement/PlayerController.gd
+++ Scripts/PlayerMovement/PlayerController.gd
@@ -1,22 +1,47 @@
 extends CharacterBody2D
 
-@export var speed = 200 # Make speed adjustable in the Inspector
+@export var speed = 10 # Make speed adjustable in the Inspector
+var direction = Vector2.ZERO
 
 func _physics_process(_delta):
-	var direction = Vector2.ZERO
+	#var direction = Vector2.ZERO
 
 	# Get movement input from both arrow keys and WASD keys
 	if Input.is_action_pressed("up") or Input.is_action_pressed("up_w"):
 		direction.y -= 1
+	elif direction.y < 0:
+		direction.y += 1
 	if Input.is_action_pressed("down") or Input.is_action_pressed("down_s"):
 		direction.y += 1
-	if Input.is_action_pressed("right") or Input.is_action_pressed("right_d"):
-		direction.x += 1
+	elif direction.y > 0:
+		direction.y -= 1
 	if Input.is_action_pressed("left") or Input.is_action_pressed("left_a"):
 		direction.x -= 1
-
+	elif direction.x < 0:
+		direction.x += 1
+	if Input.is_action_pressed("right") or Input.is_action_pressed("right_d"):
+		direction.x += 1
+	elif direction.x > 0:
+		direction.x -= 1
+	
+	direction.x = clampi(direction.x, -20, 20)
+	direction.y = clampi(direction.y, -15, 15)
+	
+	if direction.x < 0:
+		if direction.x < -10:
+			$Sprite2D.frame = 4
+		else:
+			$Sprite2D.frame = 3
+	elif direction.x > 0:
+		if direction.x > 10:
+			$Sprite2D.frame = 0
+		else:
+			$Sprite2D.frame = 1
+	else:
+		$Sprite2D.frame = 2
+		
 	# Normalize direction and apply speed
-	direction = direction.normalized()
+	#direction = direction.normalized()
 	velocity = direction * speed 
 
 	move_and_slide()
