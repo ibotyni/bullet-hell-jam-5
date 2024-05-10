42167310ed0d7507765929df78ebe7d5459dc118
Bright Tyke (J.A TAT) <zencotat@gmail.com>
2024-05-10 22:58:05 +0100

    Camera
    Camera

--- Scripts/PlayerMovement/PlayerController.gd
+++ Scripts/PlayerMovement/PlayerController.gd
@@ -20,3 +20,13 @@ func _physics_process(_delta):
 	velocity = direction * speed 
 
 	move_and_slide()
+
+	# Clamp player position within camera limits (now after move_and_slide)
+	var viewport_rect = get_viewport_rect()
+	var camera_limits = viewport_rect.size
+
+	position.x = clamp(position.x, viewport_rect.position.x + 
+	$Sprite2D.texture.get_width() / 2, viewport_rect.position.x + camera_limits.x - $Sprite2D.texture.get_width() / 2)
+	
+	position.y = clamp(position.y, viewport_rect.position.y + 
+	$Sprite2D.texture.get_height() / 2, viewport_rect.position.y + camera_limits.y - $Sprite2D.texture.get_height() / 2)
