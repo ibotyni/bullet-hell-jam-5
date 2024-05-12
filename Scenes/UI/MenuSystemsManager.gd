405620c5455c4c431bd85bd5f453c709f695f792
Bright Tyke (J.A TAT) <zencotat@gmail.com>
2024-05-12 20:54:42 +0100

    Menus working, tidied around a little
    The deletes are redundant things I am no longer using :)

---
+++ Scenes/UI/MenuSystemsManager.gd
@@ -0,0 +1,65 @@
+extends CanvasLayer
+
+@onready var MainMenu = $MainMenu
+@onready var SettingsMenu = $"Settings Menu"
+@onready var Credits = $Credits
+@onready var confirmation = $"Confirm New Game"
+
+func _ready():
+	MainMenu.visible = true
+	MainMenu.set_process(true)
+	SettingsMenu.visible = false
+	SettingsMenu.set_process(false)
+	Credits.visible = false
+	Credits.set_process(false)
+	confirmation.visible = false
+	confirmation.set_process(false)
+
+#New Game
+func _on_no_button_pressed():
+	confirmation.visible = false
+	confirmation.set_process(false)
+	MainMenu.visible = true 
+	MainMenu.set_process(true)
+
+func _on_new_game_pressed():
+	MainMenu.visible = false 
+	MainMenu.set_process(false)
+	confirmation.visible = true
+	confirmation.set_process(true)
+
+func _on_yes_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/test_hell.tscn")
+
+#Settings
+func _on_settings_pressed():
+	MainMenu.visible = false 
+	MainMenu.set_process(false)
+	SettingsMenu.visible = true
+	SettingsMenu.set_process(true)
+
+func _on_back_button_settings_pressed():
+	SettingsMenu.visible = false
+	SettingsMenu.set_process(false)
+	MainMenu.visible = true 
+	MainMenu.set_process(true)
+
+
+#Credits
+
+func _on_credits_pressed():
+	MainMenu.visible = false 
+	MainMenu.set_process(false)
+	Credits.visible = true
+	Credits.set_process(true)
+
+func _on_back_button_credits_pressed():
+	Credits.visible = false
+	Credits.set_process(false)
+	MainMenu.visible = true 
+	MainMenu.set_process(true)
+
+
+
+
+
