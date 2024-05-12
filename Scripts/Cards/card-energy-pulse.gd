7c6ceaee02ab0028e99ae1ff0f4c51e83a5a9846
fozzedout <fozzedout@gmail.com>
2024-05-12 20:59:52 +0100

    fixed setting material colour

--- Scripts/Cards/card-energy-pulse.gd
+++ Scripts/Cards/card-energy-pulse.gd
@@ -11,9 +11,9 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# weakest power is yellow, strongest is red
-	var yellowness : float = (5-power_level) * 50.0 / 255
+	var yellowness : float = ((5-power_level) * 50.0) / 255
 	$"card-frame/bullet".texture = load("res://Assets/Artwork/bullets/pulse-0{power}.png".format({"power" : power_level}))
-	$"card-frame".material.set_shader_parameter("color", Color(1,yellowness,0,1))
+	$"card-frame".material.set_shader_parameter("custom_color", Color(1,yellowness,0,1))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
