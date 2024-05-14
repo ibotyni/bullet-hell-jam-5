3326a8594f9922a1eb99977f7f0f78e0e67490de
Bright Tyke (J.A TAT) <zencotat@gmail.com>
2024-05-14 14:43:00 +0100

    enemy changes
    changes to how enemies function.

--- Scripts/EnemyTypes/Enemy.gd
+++ Scripts/EnemyTypes/Enemy.gd
@@ -4,20 +4,21 @@ signal defeated
 
 @export var speed = 50.0
 @export var health = 3
-@export var points = 100 
+@export var points = 100
 @export var damage = 3
+@export var drop_all_items_on_death: bool = false  # NEW: Boolean for controlling drops
 
-@export var drop_items: Array[PackedScene] = [] # Array to hold possible drop items
-@export var drop_chance = 0.5 
+@export var drop_items: Array[PackedScene] = []
+@export var drop_chance = 0.5
 
 @onready var sprite: Sprite2D = $Sprite2D
-@onready var health_bar: ProgressBar = $Healthbar  # Reference to your health bar node
-@onready var health_bar_timer: Timer = $HealthBarTimer   # Timer for health bar visibility
-@export var health_bar_duration: float = 2.0  # Duration the health bar is visible after damage
+@onready var health_bar: ProgressBar = $Healthbar
+@onready var health_bar_timer: Timer = $HealthBarTimer
+@export var health_bar_duration: float = 2.0
 
 func _ready():
-	health_bar.hide()  # Hide health bar initially
-	health_bar_timer.wait_time = health_bar_duration  # Set timer duration
+	health_bar.hide()
+	health_bar_timer.wait_time = health_bar_duration
 
 func _physics_process(delta):
 	global_position.y += speed * delta
@@ -28,20 +29,29 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 func die():
 	$SfxExplode.play()
-	# Drop Item Logic
-	if randf() < drop_chance and drop_items.size() > 0:
-		var random_item = drop_items.pick_random().instantiate()
-		random_item.global_position = global_position
-		get_parent().add_child(random_item)  # Add to the same container as the enemy
-	queue_free()
 
-func take_damage(amount):  
+	# --- Drop Item Logic (Modified) ---
+	
+	if drop_all_items_on_death:
+		for item_scene in drop_items:  # Drop all items
+			var new_item = item_scene.instantiate()
+			new_item.global_position = global_position
+			get_parent().add_child(new_item)
+	else:
+		if randf() < drop_chance and drop_items.size() > 0:  # Drop random item
+			var random_item = drop_items.pick_random().instantiate()
+			random_item.global_position = global_position
+			get_parent().add_child(random_item)
+
+	call_deferred("queue_free")
+
+func take_damage(amount):
 	$SfxHitHurt.play()
 	health -= amount
-	if health <= 0: 
+	if health <= 0:
 		defeated.emit(points)
-		die()
-	else: 
+		call_deferred("die")
+	else:
 		update_health_bar()
 
 func update_health_bar():
